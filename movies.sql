df190c271d66f0bc938d18260d38f154f4de2df5
Pooja Rai <67957738+poojarai2356@users.noreply.github.com>
2022-02-11 21:32:54 +0530

    Add files via upload

--- movies.sql
+++ movies.sql
@@ -1,10 +1,13 @@
 show databases;
-create database Movies;
+use movies;
 
-use Movies;
-
-create table Movies( name varchar(20),actor varchar(20), actress varchar(20), director varchar(20), year_of_release int);
+ table Movies( name varchar(50) primary key,actor varchar(20), actress varchar(20), director varchar(20), year_of_release int);
 
 insert into Movies values ("lagan", "amir khan","rachel shelly","ashutosh gwariker",2001),("3 idiot","amir","karina kapoor","rajkumar hirani",2009);
 
+select * from Movies;
+delete from Movies where name="like stars on earth";
+select * from Movies;
+
+ALTER TABLE movies MODIFY name VARCHAR(50) ;
 select * from Movies;
